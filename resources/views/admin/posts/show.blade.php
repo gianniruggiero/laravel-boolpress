72281be6d389f748fd77b6ea71c0bd6778d9555e
GIanni Ruggiero <gianni@groovecompany.it>
2020-11-09 23:22:56 +0100

    revisione views Guest e Admin

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -8,11 +8,12 @@
 
         <p><strong>Abstract: </strong> {{$post->abstract}}</p>
         <p><strong>Testo del Post: </strong> {{$post->post_text}}</p>
-        <hr>
         <p><strong>Postato il:</strong> {{$post->created_at}}</p>
-        <p><strong>Autore: </strong>{{$post->user->name}} ( {{$post->user->email}} )</p>
+        <p><strong>Autore: </strong>{{$post->user->name}}</p>
         <p><strong>Logged as: </strong> {{$post->user->email}}</p>
-
+        <br><hr>
+        <a href="{{Route('admin.posts.index')}}"><strong>&larr; BACK TO THE LIST</strong> </a>
+        
  
     </div>    
 
