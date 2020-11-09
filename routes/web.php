3221f7626499e4bd925e633b63470d7bcffc039c
GIanni Ruggiero <gianni@groovecompany.it>
2020-11-09 17:55:03 +0100

    creato controller CRUD per i posts (sotto autenticazione)

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,9 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::prefix('admin')->namespace('Admin')->middleware('auth')->group(function () {
+    Route::get('/', 'HomeController@index')->name('home');
+    Route::get('/posts', 'PostController@index')->name('index');
+});
+
+// Route::get('/home', 'HomeController@index')->name('home');
